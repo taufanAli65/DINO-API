8895982078a2d7c33e5dd3017c441e6faccfdd89
taufanAli65 <taufanali65@gmail.com>
2025-09-11 23:07:27 +0700

    feat: Add additional route handlers for kerajaan, question category, question, and quiz

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import * as dotenv from 'dotenv';
 import authRouter from './routes/auth.route';
+import kerajaanRouter from './routes/kerajaan.route';
+import questionCategoryRouter from './routes/question_category.route';
+import questionRouter from './routes/question.route';
+import quizRouter from './routes/quiz.route';
 import userRouter from './routes/user.route';
 import cors from 'cors';
 dotenv.config();
@@ -11,6 +15,10 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRouter);
+app.use("/kerajaan", kerajaanRouter);
+app.use("/question-category", questionCategoryRouter);
+app.use("/question", questionRouter);
+app.use("/quiz", quizRouter);
 app.use("/user", userRouter);
 
 app.listen(port, () => {
